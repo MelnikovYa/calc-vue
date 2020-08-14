9a8c2104f8f23c539b7c3e47e3a00104a7c68298
Yaroslav <60092939+MelnikovYa@users.noreply.github.com>
2020-08-14 20:59:25 +0300

    Update test.js

--- test.js
+++ test.js
@@ -4,10 +4,10 @@ const vue = new Vue({
     result: "",
     numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
     operations: ["+", "-", "*", "/"],
-    isButtonDisabled: false,
+    isButtonDisabled: true,
   },
   methods: {
-    input: function (char) {      
+    input: function (char) {
       this.isButtonDisabled = false;
       this.result = this.result.toString();
       this.result += char;
@@ -15,13 +15,23 @@ const vue = new Vue({
         if (vue.result[vue.result.length - 1] === item) {
           vue.isButtonDisabled = true;
         }
-      })
+      });
     },
     reset: function () {
       this.result = "";
+      this.isButtonDisabled = true;
     },
     calc: function () {
-      this.result = eval(this.result);
+      // if (this.result !== "") {
+      //   this.result = eval(this.result);
+      //   console.log("done");
+      // }
+      vue.numbers.forEach(function (num) {
+        if (vue.result[vue.result.length - 1] == num) {
+          vue.result = eval(vue.result);
+          console.log("done");
+        }
+      });
     },
   },
 });
